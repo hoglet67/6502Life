8c07401316ebbb7a8a75a19f78b4a81792a18c71
David Banks <dave@hoglet.com>
2016-10-29 15:15:41 +0100

    Added some macros and a screen->list loader
    Change-Id: I6e316449089991e5d782389bacd1c79ceef225e8

--- src/list_life.asm
+++ src/list_life.asm
@@ -1,3 +1,7 @@
+;; ************************************************************
+;; Variables
+;; ************************************************************
+
 buffer1 = &6000                 ; 6000-9FFF = 16K
 buffer2 = &A000                 ; A000-DFFF = 16K
 
@@ -10,8 +14,123 @@ prev   = &74
 next   = &76
 xx     = &78
 yy     = &7A
-bitmap = &7C        
-temp   = &7E
+bitmap = &7C
+
+;; ************************************************************
+;; Macros
+;; ************************************************************
+
+MACRO M_INCREMENT zp
+        INC zp
+        BNE nocarry
+        INC zp + 1
+.nocarry
+ENDMACRO
+
+MACRO M_INCREMENT_PTR zp
+        INC zp
+        INC zp
+        BNE nocarry
+        INC zp + 1
+.nocarry
+ENDMACRO
+
+MACRO M_ASSIGN_IF_GREATER val, ptr
+        LDY #0
+        LDA val
+        CMP (ptr), Y
+        INY
+        LDA val + 1
+        SBC (ptr), Y
+        BVC label
+        EOR #&80
+.label
+        BPL skip_assign_x
+        LDA (ptr), Y
+        STA val + 1
+        DEY
+        LDA (ptr), Y
+        STA val        
+.skip_assign_x
+ENDMACRO        
+
+MACRO M_UPDATE_BITMAP_IF_EQUAL_TO_X ptr, bmaddr, bmmask
+        LDY #0
+        LDA (ptr), Y
+        CMP xx
+        BNE skip_inc
+        INY
+        LDA (ptr), Y
+        CMP xx + 1
+        BNE skip_inc
+        LDA bmaddr
+        ORA #bmmask
+        STA bmaddr
+        M_INCREMENT_PTR ptr
+.skip_inc
+ENDMACRO
+
+MACRO M_WRITE ptr, val
+        TYA
+        PHA
+        LDY #0
+        LDA val
+        STA (ptr), Y
+        INY
+        LDA val + 1
+        STA (ptr), Y
+        INC ptr
+        INC ptr
+        BNE nocarry
+        INC ptr + 1
+.nocarry
+        PLA
+        TAY
+ENDMACRO
+
+;; ************************************************************
+;; State lookup table
+;; ************************************************************
+
+;; for(bitmap = 0; bitmap < 1<<9; bitmap++) {
+;;    for(x = y = 0; y < 9; y++)
+;;       if(bitmap & 1<<y)
+;;          x += 1;
+;;    if(bitmap & 020) {
+;;       if(x == 2 || x == 3)
+;;          state[bitmap] = LIVE;
+;;       else
+;;          state[bitmap] = DEAD;
+;;    } else {
+;;       if(x == 3)
+;;          state[bitmap] = LIVE;
+;;       else
+;;          state[bitmap] = DEAD;
+;;    }
+;; }
+                
+.state
+FOR bm, %000000000, %111111111
+    ; in BeebASM false = 0 and true = -1, hence the 9 at the start
+    x = 9 + ((bm AND &001) = 0) + ((bm AND &002) = 0) + ((bm AND &004) = 0) + ((bm AND &008) = 0) + ((bm AND &010) = 0) + ((bm AND &020) = 0) + ((bm AND &040) = 0) + ((bm AND &080) = 0) + ((bm AND &100) = 0)
+    IF ((bm AND &20) = 0)
+        IF x = 2 OR x = 3
+            EQUB LIVE
+        ELSE
+            EQUB DEAD
+        ENDIF
+    ELSE
+        IF x = 3 
+            EQUB LIVE
+        ELSE
+            EQUB DEAD
+        ENDIF
+    ENDIF
+NEXT
+
+;; ************************************************************
+;; list_life()
+;; ************************************************************
 
 ;; Args are (this) and (new)
 
@@ -32,10 +151,10 @@ temp   = &7E
 
 ;; *new = 0;
         LDY #0
+        TYA
         STA (new), Y
         INY
         STA (new), Y
-
 
 ;; for(;;) {
 
@@ -48,12 +167,9 @@ temp   = &7E
 {
         LDY #1
         LDA (new), Y
-        BPL skipinc
-        INC new        ;; new cannot be odd word aligned
-        INC new
-        BNE skipinc
-        INC new + 1
-        .skipinc         
+        BPL skip_inc
+        M_INCREMENT_PTR new
+.skip_inc  
 }
 
 
@@ -112,11 +228,7 @@ temp   = &7E
         LDA (next), Y
         ADC #0
         STA yy + 1
-        INC next
-        INC next
-        BNE nocarry_next
-        INC next + 1
-.nocarry_next
+        M_INCREMENT_PTR next
 
         JMP endif
 
@@ -133,15 +245,9 @@ temp   = &7E
         LDA (prev), Y
         CMP yy + 1
         BNE skip_inc_prev
-        INC prev
-        INC prev
-        BNE skip_inc_prev
-        INC prev + 1
+        M_INCREMENT_PTR prev
 .skip_inc_prev
-        INC yy
-        BNE nocarry_y
-        INC yy + 1
-.nocarry_y
+        M_INCREMENT yy
             
 ;;       if(*this == y)
 ;;          this++;
@@ -153,12 +259,8 @@ temp   = &7E
         LDA (this), Y
         CMP yy + 1
         BNE skip_inc_this
-        INC this
-        INC this
-        BNE skip_inc_this
-        INC this + 1
+        M_INCREMENT_PTR this
 .skip_inc_this
-
 
 ;;       if(*next == y-1)
 ;;          next++;
@@ -172,13 +274,9 @@ temp   = &7E
         ADC #0
         CMP yy + 1
         BNE skip_inc_next
-        TXA
-        CMP yy
+        CPX yy
         BNE skip_inc_next
-        INC next
-        INC next
-        BNE skip_inc_next
-        INC next + 1
+        M_INCREMENT_PTR next
 .skip_inc_next
 
 .endif
@@ -211,46 +309,12 @@ temp   = &7E
 ;;       if(x > *this)
 ;;          x = *this;
 
-{
-        LDY #0
-        LDA xx
-        CMP (this), Y
-        INY
-        LDA xx + 1
-        SBC (this), Y
-        BVC label
-        EOR #&80
-.label
-        BPL skip_assign_x
-        LDA (this), Y
-        STA xx + 1
-        DEY
-        LDA (this), Y
-        STA xx         
-.skip_assign_x
-}
+        M_ASSIGN_IF_GREATER xx, this
 
 ;;       if(x > *next)
 ;;          x = *next;
 
-{
-        LDY #0
-        LDA xx
-        CMP (next), Y
-        INY
-        LDA xx + 1
-        SBC (next), Y
-        BVC label
-        EOR #&80
-.label
-        BPL skip_assign_x
-        LDA (next), Y
-        STA xx + 1
-        DEY
-        LDA (next), Y
-        STA xx         
-.skip_assign_x
-}
+        M_ASSIGN_IF_GREATER xx, next
 
 ;;       /* end of line? */
 ;;       if(x >= 0)
@@ -271,72 +335,22 @@ temp   = &7E
 ;;             prev++;
 ;;          }
 
-{
-        LDY #0
-        LDA (prev), Y
-        CMP xx
-        BNE skip_inc_prev
-        INY
-        LDA (prev), Y
-        CMP xx + 1
-        BNE skip_inc_prev
-        LDA bitmap
-        ORA #&40
-        STA bitmap
-        INC prev
-        INC prev
-        BNE skip_inc_prev
-        INC prev + 1
-.skip_inc_prev
-}
+        M_UPDATE_BITMAP_IF_EQUAL_TO_X prev, bitmap, &40
         
 ;;          if(*this == x) {
 ;;             bitmap |= 0200;
 ;;             this++;
 ;;          }
 
-{
-        LDY #0
-        LDA (this), Y
-        CMP xx
-        BNE skip_inc_this
-        INY
-        LDA (this), Y
-        CMP xx + 1
-        BNE skip_inc_this
-        LDA bitmap
-        ORA #&80
-        STA bitmap
-        INC this
-        INC this
-        BNE skip_inc_this
-        INC this + 1
-.skip_inc_this
-}
+        M_UPDATE_BITMAP_IF_EQUAL_TO_X this, bitmap, &80
 
 ;;          if(*next == x) {
 ;;             bitmap |= 0400;
 ;;             next++;
 ;;          }
 
-{
-        LDY #0
-        LDA (next), Y
-        CMP xx
-        BNE skip_inc_next
-        INY
-        LDA (next), Y
-        CMP xx + 1
-        BNE skip_inc_next
-        LDA bitmap + 1
-        ORA #&01
-        STA bitmap + 1
-        INC next
-        INC next
-        BNE skip_inc_next
-        INC next + 1
-.skip_inc_next
-}
+        M_UPDATE_BITMAP_IF_EQUAL_TO_X next, bitmap + 1, &01
+
 
 ;;          /* what does this bitmap indicate? */
 ;;          if(state[bitmap] == LIVE)
@@ -353,11 +367,7 @@ temp   = &7E
         LDA state + &100, X
         BEQ else
 .is_live
-        INC new
-        INC new
-        BNE nocarry_new
-        INC new + 1
-.nocarry_new
+        M_INCREMENT_PTR new
         LDY #0
         LDA xx
         SEC
@@ -377,6 +387,7 @@ temp   = &7E
         LDA bitmap + 1
         ORA bitmap
         BNE endif
+
         JMP level2
 
 .endif
@@ -394,16 +405,106 @@ temp   = &7E
 
 ;;          x += 1;
 
-        INC xx
-        BNE nocarry_x
-        INC xx + 1
-.nocarry_x          
+        M_INCREMENT xx
+
+        JMP level3
 
 ;;       }
 ;;    }
 ;; }
 
-        JMP level3
+;; ************************************************************
+;; list_life_load_buffers()
+;; ************************************************************
+
+;; Initializes a list life buffer from a 256x256 screen
+;;
+
+.list_life_load_buffers
+{
+        LDA #<buffer1
+        STA this
+        LDA #>buffer1
+        STA this + 1
+
+        LDA #<buffer2
+        STA new
+        LDA #>buffer2
+        STA new + 1
+
+        LDA #<scrn_base
+        STA scrn
+        LDA #>scrn_base
+        STA scrn + 1
+
+        ;; y is positive and decreasing as you go down the screen
+        ;; 256 ... 1
+        LDA #<(&0100)
+        STA yy
+        LDA #>(&0100)
+        STA yy + 1
+
+.row_loop
+
+        LDY #&1F
+.test_blank_loop
+        LDA (scrn), Y
+        BNE row_not_blank
+        DEY
+        BPL test_blank_loop
+
+.next_row
+                  
+       LDA yy
+       SEC
+       SBC #1
+       STA yy
+       LDA yy + 1
+       SBC #0
+       STA yy + 1                  
+
+       ORA yy
+       BNE row_loop
+
+       ;; write the terminating zero
+       M_WRITE this, yy
+       
+       RTS
+
+.row_not_blank
+
+        ;; Start the row by writing the positive Y value
+        M_WRITE this, yy
+
+        ;; x is negative and increasing as you go right across the screen
+        LDA #<(&FEFF)
+        STA xx
+        LDA #>(&FEFF)
+        STA xx + 1
+
+        LDY #&00
+.byte_loop
+        LDA (scrn), Y
+        PHA
+        LDX #&07
+.bit_loop
+        PLA
+        ASL A
+        PHA
+        BCC pixel_zero
+        M_WRITE this, xx
+.pixel_zero
+        M_INCREMENT xx
+        DEX
+        BPL bit_loop
+        PLA
+        INY
+        CPY #&20
+        BNE byte_loop
+
+        BEQ next_row
+
+}
 
 
 
@@ -425,40 +526,3 @@ temp   = &7E
 
 
 
-
-
-;; for(bitmap = 0; bitmap < 1<<9; bitmap++) {
-;;    for(x = y = 0; y < 9; y++)
-;;       if(bitmap & 1<<y)
-;;          x += 1;
-;;    if(bitmap & 020) {
-;;       if(x == 2 || x == 3)
-;;          state[bitmap] = LIVE;
-;;       else
-;;          state[bitmap] = DEAD;
-;;    } else {
-;;       if(x == 3)
-;;          state[bitmap] = LIVE;
-;;       else
-;;          state[bitmap] = DEAD;
-;;    }
-;; }
-                
-.state
-FOR bm, %000000000, %111111111
-    ; in BeebASM false = 0 and true = -1, hence the 9 at the start
-    x = 9 + ((bm AND &001) = 0) + ((bm AND &002) = 0) + ((bm AND &004) = 0) + ((bm AND &008) = 0) + ((bm AND &010) = 0) + ((bm AND &020) = 0) + ((bm AND &040) = 0) + ((bm AND &080) = 0) + ((bm AND &100) = 0)
-    IF ((bm AND &20) = 0)
-        IF x = 2 OR x = 3
-            EQUB LIVE
-        ELSE
-            EQUB DEAD
-        ENDIF
-    ELSE
-        IF x = 3 
-            EQUB LIVE
-        ELSE
-            EQUB DEAD
-        ENDIF
-    ENDIF
-NEXT
